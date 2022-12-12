b412db4995d7a09ce6f71d85859e7b5af3f28c70
Malcolm Miller <entid@protonmail.com>
2022-12-12 12:13:20 +1100

    added a mint function to contract

--- GameZone.sol
+++ GameZone.sol
@@ -10,6 +10,8 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 contract GameToken is ERC20, ownable {
     uint public initialSupply;
     address payable owner = payable(msg.sender);
+    uint public totalSupply;
+    mapping(address => uint) balances;
 
     // inherit all functionality from the ERC20 base contract
     constructor(uint _initialSupply) ERC20("GAMER", "GAM") {
@@ -18,5 +20,16 @@ contract GameToken is ERC20, ownable {
         // msg.sender will receive the tokens
         _mint(owner, _initialSupply * (10 ** decimals()));
         initialSupply = _initialSupply * (10 ** decimals());
+        totalSupply += initialSupply; //update the total supply
+        balances[owner] = _initialSupply; //update the balances
+    }
+
+    // a mint function
+    // only the owner can call
+    // mint to an address
+    function mint(address _to, uint _amount) public onlyOwner {
+        payable(_to).transfer(_amount);
+        totalSupply += _amount; //update the total supply
+        balances[_to] += _amount; // update the balances mapping
     }
 }
--- README.md
+++ README.md
@@ -5,8 +5,6 @@ An ERC20 token allowing users to interact with a frontend gaming portal.
 Begin with the most basic implementation of an ERC20 and build from there.
 I want to set an initial supply and to mint that supply upon deployment. So within the constructor the \_mint() function is called and an amount is entered.
 
-## Add a mint function to my contract that only the owner can call. The owner can mint to any address.
-
 ## Add a css file to my frontend
 
 ## Add a mintMinerReward function that gives a miner 50 tokens each time a block is mined.
